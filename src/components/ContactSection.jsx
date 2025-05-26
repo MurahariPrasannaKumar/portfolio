0d8329637d56abde520693e66cf6ec02fe12a469
Murahari Prasanna Kumar <lovelytony1800@gmail.com>
2025-05-26 13:23:27 +0530

    Fix

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -56,7 +56,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Email</h4>
                   <a
-                    href="mailto:hello@gmail.com"
+                    href="lovelytony1800@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
                     lovelytony1800@gmail.com
@@ -70,7 +70,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Phone</h4>
                   <a
-                    href="tel:+11234567890"
+                    href="tel:+91 9963994989"
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
                     +91 9963994989
